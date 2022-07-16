4f65e81bcf3abf0ec4e731e76051907385c46b38
Voorgo <serbancatalin10@yahoo.com>
2022-07-16 22:03:18 +0300

    Refactor post component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,13 +1,13 @@
 import Header from "./Header";
-import Image from "./Image";
 import LikeAndComment from "./LikeAndComment";
+import Canvas from "../Canvas";
 
-const PostCard = ({ fakeUser, loading }) => {
+const PostCard = ({ user }) => {
   return (
     <div className="max-w-screen-xs mt-10 border rounded-xl border-gray-300 overflow-hidden bg-white">
-      <Header username={fakeUser.name} />
-      <Image src={fakeUser.img} loading={loading} />
-      <LikeAndComment likes={fakeUser.likes} />
+      <Header username={user.username} />
+      <Canvas />
+      <LikeAndComment likes={user.likes} />
     </div>
   );
 };
