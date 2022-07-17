35390adbc4d15bbf8fe0eff6247c887a25af9358
Voorgo <serbancatalin10@yahoo.com>
2022-07-17 11:10:44 +0300

    fix: username cannot have white space in and have to be unique

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,15 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useAuth } from "../context/authContext";
+import uniqueUsername from "../utils/uniqueUsername";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
   const { signUp } = useAuth();
-  const navigate = useNavigate();
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -27,13 +28,27 @@ const Signup = () => {
   };
 
   const handleSubmit = async (e) => {
+    setError("");
     e.preventDefault();
-    try {
-      await signUp(email, password, name, username);
 
-      navigate("/");
-    } catch (e) {
-      console.log(e);
+    let isUnique = await uniqueUsername(username);
+    if (username.length < 3) {
+      setError("Username should have at least 3 letters");
+      return;
+    }
+    if (!/^\S{3,}$/.test(username)) {
+      setError("Name cannot contain whitespace");
+      return;
+    }
+    if (!isUnique) {
+      try {
+        await signUp(email, password, name, username, setError);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setError("Username already taken");
+      return;
     }
   };
 
@@ -43,11 +58,19 @@ const Signup = () => {
   return (
     <div className="container mx-auto flex items-center max-w-screen-md h-screen justify-center p-5">
       <div className="w-2/4 hidden md:flex">
-        <img src={"/images/iphone-with-app.jpg"} alt="app preview on phone" />
+        <img
+          src={require("../images/iphone-with-app.jpg")}
+          alt="app preview on phone"
+        />
       </div>
       <div className="flex gap-4 flex-col max-w-lg w-full md:w-[45%]">
         <div className="flex flex-col  xs:bg-white xs:px-10 xs:pb-10 xs:border xs:border-gray-primary xs:mb-4 xs:rounded">
           <h1 className="font-nosfier text-5xl text-center my-10">Artismo</h1>
+          {error ? (
+            <div className="text-center text-xl border border-red-500 py-3 mb-5 bg-red-500 text-white font-bold">
+              {error}
+            </div>
+          ) : null}
           <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
             <input
               onChange={handleUsername}
@@ -56,6 +79,7 @@ const Signup = () => {
               aria-label="Enter username"
               className="outline-2 outline-blue-500 text-2xl p-3 pl-5 overflow-ellipsis
                  border border-gray-400 rounded w-full"
+              required
             />
             <input
               onChange={handleName}
@@ -64,6 +88,7 @@ const Signup = () => {
               aria-label="Enter full name"
               className="outline-2 outline-blue-500 text-2xl p-3 pl-5 overflow-ellipsis
                border border-gray-400 rounded w-full"
+              required
             />
             <input
               onChange={handleEmail}
@@ -72,6 +97,7 @@ const Signup = () => {
               aria-label="Enter email address"
               className="outline-2 outline-blue-500 text-2xl p-3 pl-5 overflow-ellipsis
                border border-gray-400 rounded w-full"
+              required
             />
             <input
               onChange={handlePassword}
@@ -80,6 +106,7 @@ const Signup = () => {
               aria-label="Enter password"
               className="outline-2 outline-blue-500 text-2xl p-3 pl-5 overflow-ellipsis
                border border-gray-400 rounded w-full"
+              required
             />
             <button className="px-6 py-3  bg-blue-600 text-white text-2xl font-bold rounded">
               Sign Up
