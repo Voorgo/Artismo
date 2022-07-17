f072707d5842e596bab5a9da2507d1c4e9c0550f
Voorgo <serbancatalin10@yahoo.com>
2022-07-17 11:05:48 +0300

    Add avatar photo on post

--- src/components/post/Header.js
+++ src/components/post/Header.js
@@ -3,7 +3,20 @@ import { Link } from "react-router-dom";
 const Header = ({ username }) => {
   return (
     <div className="flex items-center gap-5 p-5 text-xl font-bold border-b rounded-xl border-gray-300">
-      <div className="w-12 h-12 bg-black rounded-full"></div>
+      <div className="w-12 h-12 rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-full w-full"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
       <Link to={`/profile/${username}`}>{username}</Link>
     </div>
   );
