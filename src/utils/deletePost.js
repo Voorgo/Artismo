676a058deaca2c880e31d40c352631671e98bbad
Voorgo <serbancatalin10@yahoo.com>
2022-07-26 00:42:05 +0300

    Refactor

--- src/utils/deletePost.js
+++ src/utils/deletePost.js
@@ -1,22 +1,15 @@
 import { db } from "../firebase";
-import { updateDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc } from "firebase/firestore";
 import { getStorage, ref, deleteObject } from "firebase/storage";
 
-const deletePost = async (deletePost, post, username, allPosts) => {
+const deletePost = async (post) => {
   const storage = getStorage();
   const imageRef = ref(storage, post.path);
   deleteObject(imageRef)
-    .then(() => {
-      deletePost((prev) => !prev);
-    })
+    .then(() => {})
     .catch((error) => console.log(error));
   try {
-    let filtered = allPosts.filter(
-      (postAndLikes) => postAndLikes.path !== post.path
-    );
-    await updateDoc(doc(db, "posts", `${username}`), {
-      imageSrcAndLikes: filtered,
-    });
+    await deleteDoc(doc(db, "posts", `${post.id}`));
   } catch (error) {
     console.log(error);
   }
